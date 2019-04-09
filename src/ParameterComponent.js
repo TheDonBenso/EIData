df75d750a84b36465afd90e925d9432f29e8045c
TheDonBenso <b.m.nyakabwa@gmail.com>
2019-04-09 22:49:54 +0200

    autofill error
    imported a non helping library

--- src/ParameterComponent.js
+++ src/ParameterComponent.js
@@ -1,14 +1,27 @@
 import React, { Component } from 'react';
+import Select from 'react-select';
+
+
+const tradeType = [
+    { label: "Export Data", value: 1 },
+    { label: "Import Data", value: 2 },
+    { label: "Both Data", value: 3 },
+  ];
+
 
 class ParameterComponent extends Component
 {
     render(){
 
         return (
-            <div className="Header">
-                <h1>Hello World from ParameterComponent</h1>
+            <div>
+                <div className="Header">
+                    <h1>Dashboard</h1>
+                </div>
+                <div>
+                   <Select options={tradeType} />
+                </div>
             </div>
-
         );
     
     };
