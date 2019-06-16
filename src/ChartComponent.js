29ee792a393dd46c810a2c630cf82a317e9be605
TheDonBenso <b.m.nyakabwa@gmail.com>
2019-06-16 16:18:57 +0200

    chart data binding
    chart data has been bound, however the useeffect hook is rendering over and over.
    
    solve the issue soon

--- src/ChartComponent.js
+++ src/ChartComponent.js
@@ -14,67 +14,57 @@ export const ChartComponent = (props) =>
     const {selectedCountries, getCountry, tradeType} = useController();
     const [country,setCountry] = useState({});
     const [tradeKeys, setTradeKeys] = useState({});
-    const [importData, setImportData] = useState([]);
-    const [exportData, setExportData] = useState([]);
+    const [importData, setImportData] = useState([
+     
+    ]);
+    const [exportData, setExportData] = useState([
+      {
+        year : "",
+        total : 0
+      }
+    ]);
 
     useEffect(()=>{
             // (the first one, and every one after that)
-            console.log('render! Country: '+ props.name +' id : '+ props.id +' type: '+ tradeType);
+         //   console.log('render! Country: '+ props.name +' id : '+ props.id +' type: '+ tradeType);
             setCountry(state.Countries[props.id]);
-            console.log(country);
-           // setTradeKeys(Object.keys(state.Countries[props.id].tradeType));
-           // console.log(tradeKeys);
 
            var mykeys =Object.keys(country);
-          // console.log(mykeys);
 
            if((tradeType !== null) && (tradeType === "import"))
            {
                 var imports = country[mykeys[3]];
-               
-                console.log(imports);
                 
-                Object.values(imports).map((year)=>{
-                  var totals = Object.values(year).reduce((total, curvalue)=> total+=curvalue);
-                  //setImportData((prev)=> ({...prev,totals}));
-                  console.log(totals);
+                Object.values(imports).map((_year, _index, _importkeys) => {
+                  var usekey =  Object.keys(imports);
+                  var totals = Object.values(_year).reduce((total, curvalue)=> total+=curvalue);
+                  return setImportData(prevImportData => [...prevImportData, { year:  usekey[_index], total: totals}]);
+                  
                 });
-                console.log(importData);
                 
            }
            if((tradeType !== null) && (tradeType === "export"))
            {
             var exports = country[mykeys[2]];
             setExportData(exports);
-            console.log(exports);
+        //    console.log(exports);
            }
            if((tradeType !== null) && (tradeType === "both"))
            {
-            var imports = country[mykeys[3]];
-            var exports = country[mykeys[2]];
+          //  var imports = country[mykeys[3]];
+            //var exports = country[mykeys[2]];
 
-            console.log(imports);
-            console.log(exports);
+         
            }
 
             // If you want to implement componentWillUnmount,
             // return a function from here, and React will call
             // it prior to unmounting.
-            return () => console.log('unmounting...');
-            }
-    , [props.name, props.id, tradeType, state.Countries, country,tradeKeys, importData, exportData]
+            return () => console.log('chart component unmounting...');
+      }, [props.id, importData,tradeType, state.Countries, country]//props.name,  tradeKeys,, exportData]
     );
 
-    function sumTrade(tradedata) {
-
-        let sum = 0;
-        for (let volume of Object.values(tradedata)) {
-          sum += volume;
-        }
-      
-        return sum; // 650
-      }
-
+  
 
         return (
             <div className="chart-box">
@@ -86,20 +76,19 @@ export const ChartComponent = (props) =>
                <BarChart
                     width={500}
                     height={300}
-                    //data={importData}
+                    data={importData}
                     margin={{
                     top: 5, right: 5, left: 5, bottom: 5,
                     }}
                 >
-                    
-        <CartesianGrid strokeDasharray="3 3" />
-        <XAxis dataKey="Import"/>
-        <YAxis />
-        <Tooltip />
-        <Legend />
-        <Bar dataKey="Import" fill="#8884d8" />
-        <Bar dataKey="Export" fill="#82ca9d" />
-     
+                            
+                <CartesianGrid strokeDasharray="3 3" />
+                <XAxis dataKey="year"/>
+                <YAxis dataKey="total"/>
+                <Tooltip />
+                <Legend />
+                <Bar dataKey="total" fill="#8884d8" />
+            
 
                 </BarChart>
 
